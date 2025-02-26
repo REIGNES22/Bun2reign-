fae5a5ec1e85719a12591f923f161738d6a30424
REIGNES22 <nejoreigneth22@gmail.com>
2025-02-26 10:57:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: Bun2reign,bot // Your name
-  ownerNumber:  //Your09169126847 number
+  botOwner: "Bun2reign,bot // Your name
+  ownerNumber: "+2349169126847 //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
